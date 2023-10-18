43917570388edfabf7311f182592a807c3c196cc
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-10-18 13:24:57 -0300

    Added new task button to create Tasks
    Added the DOMManipulation method to create the elements and the js code to create the task object.
    
    Changed the placement of new project and task buttons and current projects on the page.

--- src/DOMManipulation.js
+++ src/DOMManipulation.js
@@ -8,7 +8,7 @@ export function createNewProjectForm() {
   nameLabel.setAttribute('for', 'project-name');
   nameLabel.textContent = 'Project Name';
 
-  const nameInput = document.createElement('input')
+  const nameInput = document.createElement('input');
   nameInput.setAttribute('name', 'project-name');
   nameInput.setAttribute('id', 'project-name');
   nameInput.setAttribute('type', 'text');
@@ -23,13 +23,14 @@ export function createNewProjectForm() {
   legend.textContent = 'Priority';
 
   const radioDiv = document.createElement('div');
-  radioDiv.classList.add('radio-container');
+  radioDiv.classList.add('project-radio-container');
 
   const lowPriorityInput = document.createElement('input');
   lowPriorityInput.setAttribute('type', 'radio');
   lowPriorityInput.setAttribute('id', 'low-priority');
   lowPriorityInput.setAttribute('name', 'priority');
   lowPriorityInput.setAttribute('value', 'low-priority');
+  lowPriorityInput.checked = true;
 
   const lowPriorityLabel = document.createElement('label');
   lowPriorityLabel.setAttribute('for', 'low-priority');
@@ -65,6 +66,134 @@ export function createNewProjectForm() {
   priorityDiv.appendChild(legend);
   priorityDiv.appendChild(radioDiv);
 
+  const createNewProjectButton = document.createElement('button');
+  createNewProjectButton.classList.add('create-new-project-button');
+  createNewProjectButton.textContent = 'Create';
+
   newProjectDiv.appendChild(nameDiv);
   newProjectDiv.appendChild(priorityDiv);
+  newProjectDiv.appendChild(createNewProjectButton);
+}
+
+export function deleteNewProjectForm() {
+  const newProjectForm = document.querySelector('.new-project-form');
+  while(newProjectForm.firstChild) {
+    newProjectForm.removeChild(newProjectForm.lastChild);
+  }
+}
+
+export function createNewTaskForm() {
+  const newTaskDiv = document.querySelector('.new-task-form');
+
+  const nameDiv = document.createElement('div');
+  nameDiv.classList.add('task-name-container');
+
+  const nameLabel = document.createElement('label');
+  nameLabel.setAttribute('for', 'task-name');
+  nameLabel.textContent = 'Task Name';
+
+  const nameInput = document.createElement('input');
+  nameInput.setAttribute('name', 'task-name');
+  nameInput.setAttribute('id', 'task-name');
+  nameInput.setAttribute('type', 'text');
+  
+  nameDiv.appendChild(nameLabel);
+  nameDiv.appendChild(nameInput);
+
+  const descriptionDiv = document.createElement('div');
+  descriptionDiv.classList.add('task-description-container');
+
+  const descriptionLabel = document.createElement('label');
+  descriptionLabel.setAttribute('for', 'task-description');
+  descriptionLabel.textContent = 'Description';
+
+  const descriptionInput = document.createElement('input');
+  descriptionInput.setAttribute('name', 'task-description');
+  descriptionInput.setAttribute('id', 'task-description');
+  descriptionInput.setAttribute('type', 'textarea');
+
+  descriptionDiv.appendChild(descriptionLabel);
+  descriptionDiv.appendChild(descriptionInput);
+
+  const dateDiv = document.createElement('div');
+  dateDiv.classList.add('task-date-container');
+
+  const dateLabel = document.createElement('label');
+  dateLabel.setAttribute('for', 'task-date');
+  dateLabel.textContent = 'Due date';
+
+  const dateInput = document.createElement('input');
+  dateInput.setAttribute('name', 'task-date');
+  dateInput.setAttribute('id', 'task-date');
+  dateInput.setAttribute('type', 'date');
+
+  dateDiv.appendChild(dateLabel);
+  dateDiv.appendChild(dateInput);
+
+  const priorityDiv = document.createElement('div');
+  priorityDiv.classList.add('task-priority-container');
+
+  const legend = document.createElement('legend');
+  legend.textContent = 'Priority';
+
+  const radioDiv = document.createElement('div');
+  radioDiv.classList.add('task-radio-container');
+
+  const lowPriorityInput = document.createElement('input');
+  lowPriorityInput.setAttribute('type', 'radio');
+  lowPriorityInput.setAttribute('id', 'low-priority');
+  lowPriorityInput.setAttribute('name', 'priority');
+  lowPriorityInput.setAttribute('value', 'low-priority');
+  lowPriorityInput.checked = true;
+
+  const lowPriorityLabel = document.createElement('label');
+  lowPriorityLabel.setAttribute('for', 'low-priority');
+  lowPriorityLabel.textContent = 'Low';
+
+  const mediumPriorityInput = document.createElement('input');
+  mediumPriorityInput.setAttribute('type', 'radio');
+  mediumPriorityInput.setAttribute('id', 'medium-priority');
+  mediumPriorityInput.setAttribute('name', 'priority');
+  mediumPriorityInput.setAttribute('value', 'medium-priority');
+  
+  const mediumPriorityLabel = document.createElement('label');
+  mediumPriorityLabel.setAttribute('for', 'medium-priority');
+  mediumPriorityLabel.textContent = 'Medium';
+
+  const highPriorityInput = document.createElement('input');
+  highPriorityInput.setAttribute('type', 'radio');
+  highPriorityInput.setAttribute('id', 'high-priority');
+  highPriorityInput.setAttribute('name', 'priority');
+  highPriorityInput.setAttribute('value', 'high-priority');
+  
+  const highPriorityLabel = document.createElement('label');
+  highPriorityLabel.setAttribute('for', 'high-priority');
+  highPriorityLabel.textContent = 'High';
+
+  radioDiv.appendChild(lowPriorityInput);
+  radioDiv.appendChild(lowPriorityLabel);
+  radioDiv.appendChild(mediumPriorityInput);
+  radioDiv.appendChild(mediumPriorityLabel);
+  radioDiv.appendChild(highPriorityInput);
+  radioDiv.appendChild(highPriorityLabel);
+
+  priorityDiv.appendChild(legend);
+  priorityDiv.appendChild(radioDiv);
+
+  const createNewTaskButton = document.createElement('button');
+  createNewTaskButton.classList.add('create-new-task-button');
+  createNewTaskButton.textContent = 'Create';
+
+  newTaskDiv.appendChild(nameDiv);
+  newTaskDiv.appendChild(descriptionDiv);
+  newTaskDiv.appendChild(dateDiv);
+  newTaskDiv.appendChild(priorityDiv);
+  newTaskDiv.appendChild(createNewTaskButton);
+}
+
+export function deleteNewTaskForm() {
+  const newTaskForm = document.querySelector('.new-task-form');
+  while(newTaskForm.firstChild) {
+    newTaskForm.removeChild(newTaskForm.lastChild);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./normalize.css";
 import "./style.css";
-import { createNewProjectForm } from "./DOMManipulation.js";
+import { createNewProjectForm, deleteNewProjectForm, createNewTaskForm, deleteNewTaskForm } from "./DOMManipulation.js";
 
 const currentProjects = [];
 const finishedProjects = [];
@@ -9,7 +9,77 @@ class Project {
   constructor(name, priority) {
     this.name = name;
     this.priority = priority;
+
+    const tasks = [];
   }
 }
 
-document.querySelector('.new-project-button').addEventListener('click', createNewProjectForm);
+class Task {
+  constructor(name, description, date, priority) {
+    this.name = name;
+    this.description = description;
+    this.date = date;
+    this.priority = priority;
+  }
+}
+
+document.querySelector('.new-project-button').addEventListener('click', () => {
+  if(document.querySelector('.new-task-form').firstChild) {
+    deleteNewTaskForm();
+  }
+
+  if(document.querySelector('.new-project-form').firstChild) {
+    deleteNewProjectForm();
+  } else {
+    createNewProjectForm();
+    createNewProjectButtonEvent();
+  }
+});
+
+function createNewProjectButtonEvent() {
+  document.querySelector('.create-new-project-button').addEventListener('click', () => {
+    let newProject;
+    if(document.querySelector('#low-priority').checked) {
+      newProject = new Project(document.querySelector('#project-name').value, document.querySelector('#low-priority').value);
+    } else if(document.querySelector('#medium-priority').checked) {
+      newProject = new Project(document.querySelector('#project-name').value, document.querySelector('#medium-priority').value);
+    } else if(document.querySelector('#high-priority').checked) {
+      newProject = new Project(document.querySelector('#project-name').value, document.querySelector('#high-priority').value);
+    }
+    currentProjects.push(newProject);
+    console.log(newProject);
+    deleteNewProjectForm();
+  });
+}
+
+document.querySelector('.new-task-button').addEventListener('click', () => {
+  if(document.querySelector('.new-project-form').firstChild) {
+    deleteNewProjectForm();
+  }
+
+  if(document.querySelector('.new-task-form').firstChild) {
+    deleteNewTaskForm();
+  } else {
+    createNewTaskForm();
+    createNewTaskButtonEvent();
+  }
+});
+
+function createNewTaskButtonEvent() {
+  document.querySelector('.create-new-task-button').addEventListener('click', () => {
+    let newTask;
+    if(document.querySelector('#low-priority').checked) {
+      newTask = new Task(document.querySelector('#task-name').value, document.querySelector('#task-description').value, 
+                         document.querySelector('#task-date').value, document.querySelector('#low-priority').value);
+    } else if(document.querySelector('#medium-priority').checked) {
+      newTask = new Task(document.querySelector('#task-name').value, document.querySelector('#task-description').value, 
+                         document.querySelector('#task-date').value, document.querySelector('#medium-priority').value);
+    } else if(document.querySelector('#high-priority').checked) {
+      newTask = new Task(document.querySelector('#task-name').value, document.querySelector('#task-description').value, 
+                         document.querySelector('#task-date').value, document.querySelector('#high-priority').value);
+    }
+    // currentProjects.push(newProject);
+    console.log(newTask);
+    deleteNewTaskForm();
+  });
+}
